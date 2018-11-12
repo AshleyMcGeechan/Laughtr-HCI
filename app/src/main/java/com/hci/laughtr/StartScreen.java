8198b8945b3ca32749a677dfb1eb37f99ef9693a
Dasha <darya_shum@live.co.uk>
2018-11-12 20:11:25 +0000

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/hci/laughtr/StartScreen.java
+++ app/src/main/java/com/hci/laughtr/StartScreen.java
@@ -2,6 +2,8 @@ package com.hci.laughtr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        final ImageButton button = findViewById(R.id.ages4_7);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.mood_ages4_7);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/hci/laughtr/ToddlerMood.java
@@ -0,0 +1,12 @@
+package com.hci.laughtr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ToddlerMood extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mood_ages4_7);
+    }
+}
